1104cf700588606983aae7e5993e25d90715248e
Rocket47 <romtswork@gmail.com>
2020-09-07 23:48:59 +0300

    Initial verison of the project

---
+++ FinancialAccounting/Controllers/UsersController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace FinancialAccounting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        UsersContext db;
+        public UsersController(UsersContext context)
+        {
+            db = context;
+            if (!db.Users.Any())
+            {
+                db.Users.Add(new User { income = "1000$", expence = "700$" });
+                db.Users.Add(new User { income = "1500$", expence = "1000$" });
+                db.SaveChanges();
+            }
+        }
+        // GET: api/<OperationsController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> Get()
+        {
+            return await db.Users.ToListAsync();
+        }
+
+        // GET api/<OperationsController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> Get(int id)
+        {
+            User user = await db.Users.FirstOrDefaultAsync(x => x.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(user);
+        }
+
+        // POST api/<OperationsController>
+        [HttpPost]
+        public async Task<ActionResult<User>> Post(User user)
+        {
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            db.Users.Add(user);
+            await db.SaveChangesAsync();
+            return Ok(user);
+        }
+
+        // PUT api/<OperationsController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult<User>> Put(User user)
+        {
+            if (user == null)
+            {
+                return BadRequest();
+            }
+            if (!db.Users.Any(x => x.Id == user.Id))
+            {
+                return NotFound();
+            }
+
+            db.Update(user);
+            await db.SaveChangesAsync();
+            return Ok(user);
+        }
+
+        // DELETE api/<OperationsController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<User>> Delete(int id)
+        {
+            User user = db.Users.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            db.Users.Remove(user);
+            await db.SaveChangesAsync();
+            return Ok(user);
+        }
+    }
+}
---
+++ FinancialAccounting/Models/User.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FinancialAccounting
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string income { get; set; }
+        public string expence { get; set; }
+    }
+}
